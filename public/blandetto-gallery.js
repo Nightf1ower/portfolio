fcb3c276cef835bda6173bcadcd626727f329b32
Nightf1ower <mudak445@gmail.com>
2026-06-24 16:43:19 +0300

    Refresh ZNY no double scrollbar loader

--- public/blandetto-gallery.js
+++ public/blandetto-gallery.js
@@ -3,7 +3,7 @@
   // The Blandetto section is now handled by /blandetto-static.js to avoid mobile loading lockups.
   if (!document.querySelector('script[src^="/zny-gallery.js"]')) {
     const script = document.createElement('script');
-    script.src = '/zny-gallery.js?v=zny-4';
+    script.src = '/zny-gallery.js?v=zny-5';
     script.async = false;
     (document.currentScript || document.body).after(script);
   }
