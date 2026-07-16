1ebef4de852c57357f3410cfe8b36ccf40dff12c
Nightf1ower <mudak445@gmail.com>
2026-07-16 10:41:47 +0300

    Load ZNY v7 gallery updates

--- public/blandetto-gallery.js
+++ public/blandetto-gallery.js
@@ -1,9 +1,9 @@
 (() => {
   // Disabled old dynamic GitHub API loader.
   // The Blandetto section is now handled by /blandetto-static.js to avoid mobile loading lockups.
-  if (!document.querySelector('script[src^="/zny-gallery.js"]')) {
+  if (!document.querySelector('script[src^="/zny-gallery-v7.js"]')) {
     const script = document.createElement('script');
-    script.src = '/zny-gallery.js?v=zny-6';
+    script.src = '/zny-gallery-v7.js?v=zny-7';
     script.async = false;
     (document.currentScript || document.body).after(script);
   }
