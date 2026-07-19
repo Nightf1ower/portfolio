0cfbd65975f7ec6cc04b593a8d8f5e758a5c74dd
Nightf1ower <mudak445@gmail.com>
2026-07-19 19:18:13 +0300

    Load stabilized Fable gallery

--- public/blandetto-gallery.js
+++ public/blandetto-gallery.js
@@ -11,7 +11,7 @@
 
   loadScript('script[src^="/global-modal-header-fix.js"]', '/global-modal-header-fix.js?v=modal-head-3');
   loadScript('script[src^="/zny-gallery-v7.js"]', '/zny-gallery-v7.js?v=zny-8');
-  loadScript('script[src^="/fable-gallery.js"]', '/fable-gallery.js?v=fable-2');
+  loadScript('script[src^="/fable-gallery.js"]', '/fable-gallery.js?v=fable-3');
   loadScript('script[src^="/project9006-layout.js"]', '/project9006-layout.js?v=9006-layout-4');
   loadScript('script[src^="/merch-ui-performance-v12.js"]', '/merch-ui-performance-v12.js?v=merch-ui-13');
   loadScript('script[src^="/merch-gallery-v10.js"]', '/merch-gallery-v10.js?v=merch-16');
