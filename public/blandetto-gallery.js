5df63d458ad60ed8250af58811cf23f8802a18c1
Nightf1ower <mudak445@gmail.com>
2026-07-19 13:16:21 +0300

    Load Nightflower Playground module

--- public/blandetto-gallery.js
+++ public/blandetto-gallery.js
@@ -15,6 +15,7 @@
   loadScript('script[src^="/project9006-layout.js"]', '/project9006-layout.js?v=9006-layout-1');
   loadScript('script[src^="/merch-ui-performance-v12.js"]', '/merch-ui-performance-v12.js?v=merch-ui-13');
   loadScript('script[src^="/merch-gallery-v10.js"]', '/merch-gallery-v10.js?v=merch-13');
+  loadScript('script[src^="/nightflower-playground.js"]', '/nightflower-playground.js?v=playground-1');
 
   const updateNavigation = () => {
     const language = localStorage.getItem('site-language') || 'en';
