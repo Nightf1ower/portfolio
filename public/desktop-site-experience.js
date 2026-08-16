3ae6af21b510bd9c9a23424eb4c6538013ba9b42
Nightf1ower <mudak445@gmail.com>
2026-08-16 13:16:42 +0300

    Load final reliable project navigation runtime

--- public/desktop-site-experience.js
+++ public/desktop-site-experience.js
@@ -1,6 +1,6 @@
 (() => {
-  if (window.__desktopSiteExperienceLoaderV16) return;
-  window.__desktopSiteExperienceLoaderV16 = true;
+  if (window.__desktopSiteExperienceLoaderV17) return;
+  window.__desktopSiteExperienceLoaderV17 = true;
   window.__desktopSiteExperienceV1 = true;
 
   const load = (src, marker) => {
@@ -22,4 +22,5 @@
   load('/blandetto-print-layout-restore.js?v=blandetto-print-layout-restore-1', 'blandetto-print-layout-restore-1');
   load('/project9006-standard-intro-fix.js?v=project9006-standard-intro-fix-1', 'project9006-standard-intro-fix-1');
   load('/portfolio-themed-intro-restore.js?v=portfolio-themed-intro-restore-2', 'portfolio-themed-intro-restore-2');
+  load('/project-navigation-final-fix.js?v=project-navigation-final-fix-1', 'project-navigation-final-fix-1');
 })();
