d1409708b3a160821532332a8f900e676ed0ecb3
Nightf1ower <mudak445@gmail.com>
2026-09-09 13:30:32 +0300

    Load updated mobile visual noise fix

--- public/desktop-site-experience.js
+++ public/desktop-site-experience.js
@@ -26,5 +26,5 @@
   load('/blandetto-print-layout-restore.js?v=blandetto-print-layout-restore-1', 'blandetto-print-layout-restore-1');
   load('/homepage-project-curation.js?v=homepage-project-curation-2', 'homepage-project-curation-2');
   load('/portfolio-project-consistency.js?v=portfolio-project-consistency-1', 'portfolio-project-consistency-1');
-  load('/visual-noise-mobile-v2.js?v=visual-noise-mobile-2', 'visual-noise-mobile-2');
+  load('/visual-noise-mobile-v2.js?v=visual-noise-mobile-3', 'visual-noise-mobile-3');
 })();
